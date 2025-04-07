dc59172a72b42cc6778685053b006d4597f0a1a6
Magik Bear <magik.bear.dev@outlook.com>
2025-04-08 00:39:23 +0200

    Added ToggleTraceNotifyState.h and ToggleTraceNotifyState.cpp + added C++ code for Animation Notify States

---
+++ Source/ActionCombatGame/Public/Animations/ToggleTraceNotifyState.h
@@ -0,0 +1,30 @@
+// Magik Bear Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "ToggleTraceNotifyState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONCOMBATGAME_API UToggleTraceNotifyState : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NotifyBegin(
+		USkeletalMeshComponent* MeshComp,
+		UAnimSequenceBase* Animation,
+		float TotalDuration,
+		const FAnimNotifyEventReference& EventReference
+		) override;
+
+	virtual void NotifyEnd(
+		USkeletalMeshComponent* MeshComp,
+		UAnimSequenceBase* Animation,
+		const FAnimNotifyEventReference& EventReference
+	) override;
+};
