409da6c65a69aeb0fca8dbf56b88d2e791e1ca4f
Magik Bear <magik.bear.dev@outlook.com>
2025-04-08 15:20:20 +0200

    Added C++ code for Boss Character Health

--- Source/ActionCombatGame/Public/Characters/StatsComponent.h
+++ Source/ActionCombatGame/Public/Characters/StatsComponent.h
@@ -28,5 +28,6 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	UFUNCTION(BlueprintCallable)
+	void ReduceHealth(float Amount);
 };
