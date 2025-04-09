9e6f73f00acda8bf14dd331a06bafc04fcec562a
Magik Bear <magik.bear.dev@outlook.com>
2025-04-09 17:16:25 +0200

    Added C++ code for reducing stamina for player.

--- Source/ActionCombatGame/Private/Characters/MainCharacter.cpp
+++ Source/ActionCombatGame/Private/Characters/MainCharacter.cpp
@@ -54,3 +54,8 @@ float AMainCharacter::GetDamage()
 	return StatsComp->Stats[EStat::Strength];
 }
 
+bool AMainCharacter::HasEnoughStamina(float Cost)
+{
+	return StatsComp->Stats[EStat::Stamina] >= Cost;
+}
+
