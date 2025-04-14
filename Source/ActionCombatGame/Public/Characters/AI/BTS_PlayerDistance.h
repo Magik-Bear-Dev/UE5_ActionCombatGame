5edf6d4ba5c0aac4b5b9bf6c1ccae3e49fac2284
Magik Bear <magik.bear.dev@outlook.com>
2025-04-14 21:44:38 +0200

    Added C++ code to create behavior tree services

---
+++ Source/ActionCombatGame/Public/Characters/AI/BTS_PlayerDistance.h
@@ -0,0 +1,24 @@
+// Magik Bear Productions
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTS_PlayerDistance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONCOMBATGAME_API UBTS_PlayerDistance : public UBTService
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void	
+		TickNode(
+		UBehaviorTreeComponent& OwnerComp,
+		uint8* NodeMemory,
+		float DeltaSeconds
+	) override;
+};
